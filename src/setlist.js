2cbe4ffd31b985062448f22a697f6da252105edb
cleancodernz <cleancode@hotmail.com>
2024-10-18 14:40:10 +1300

    more updates

--- src/setlist.js
+++ src/setlist.js
@@ -1,36 +1,32 @@
 // Fetch songs from the Azure Function
 async function fetchSongs() {
+    alert('fetching songs');
     const response = await fetch('https://setlistaccess.azurewebsites.net/api/setlistaccess?code=F41koAeIxKb2LHORHlumzuQxdU8RiIKjbV87FVGbd_ccAzFu3S5Nfw%3D%3D');
     const songs = await response.json();
+    alert('fetching songs' + songs);
     return songs;
 }
 
 // Display the songs in a list that supports drag-and-drop reordering
-// function displaySongs(songs) {
-//     const setlist = document.getElementById('setlist');
-//     setlist.innerHTML = ''; // Clear the list
+function displaySongs(songs) {
+    // Generate song tiles
+    const tileContainer = document.getElementById('tileContainer');
+    songs.forEach((song, index) => {
+        const songTile = document.createElement('div');
+        songTile.classList.add('song-tile');
+        songTile.dataset.index = index;
+        songTile.textContent = `${song.song_name} - ${song.artist} (${song.length} min, ${song.release_year})`;
+        tileContainer.appendChild(songTile);
+    });
 
-//     songs.forEach((song, index) => {
-//         const listItem = document.createElement('li');
-//         listItem.className = 'song-item';
-//         listItem.setAttribute('data-id', index); // Use index as unique identifier
-//         listItem.textContent = `${song.song_name} - ${song.artist} (${song.length} min, ${song.release_year})`;
-//         setlist.appendChild(listItem);
-
-//         const songs = [
-//             { song_name: 'Song 1', artist: 'Artist 1', length: 3, release_year: 2020 },
-
-//     });
-
-//     // Initialize Sortable.js to allow drag-and-drop reordering
-//     Sortable.create(setlist, {
-//         animation: 150, // Animation speed in ms when moving items
-//         onEnd: function (/**Event*/evt) {
-//             // Logic after the song order has been changed
-//             console.log('New order:', getCurrentSetlistOrder());
-//         }
-//     });
-// }
+    // Make the song tiles sortable using Sortable.js
+    const sortable = new Sortable(tileContainer, {
+        animation: 150,
+        onEnd: function () {
+            console.log("Order has been changed");
+        }
+    });
+}
 
 // Get the current order of the setlist based on the DOM
 function getCurrentSetlistOrder() {
@@ -40,7 +36,7 @@ function getCurrentSetlistOrder() {
 }
 
 // Save the reordered setlist (for demonstration, logs the result)
-document.getElementById('save-setlist').addEventListener('click', () => {
+document.getElementById('savesetlist').addEventListener('click', () => {
     const reorderedSetlist = getCurrentSetlistOrder();
     console.log('Setlist saved:', reorderedSetlist);
     // You can send this reordered setlist back to your Azure Function or save it in local storage
