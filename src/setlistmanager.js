097000f2f3efac17e053d87657ba18c68f17b4ae
cleancodernz <cleancode@hotmail.com>
2024-10-24 21:35:55 +1300

    fixed syntax error

--- src/setlistmanager.js
+++ src/setlistmanager.js
@@ -139,7 +139,7 @@ async function addSongToDatabase(song) {
 
 // Format song duration (from s to mm:ss)
 function formatDuration(duration) {
-    durationMs = (duration*1000)
+    const durationMs = (duration*1000)
     const minutes = Math.floor(durationMs / 60000);
     const seconds = Math.floor((durationMs % 60000) / 1000).toFixed(0);
     return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
